0d59b30e2a5d58a0e4a2ae489cd830a3520d05c0
Baltrushaitis Tomas <tbaltrushaitis@gmail.com>
2017-06-15 06:46:41 +0300

    Code refactoring to make it easier to perform automated test. Index.js now responsible for command-line parameters processing and initiation of Battle simulation. Controlling of game process moved to Emcee module.

--- index.js
+++ index.js
@@ -15,90 +15,51 @@
      */
 
     var fs      =   require('fs');
+    var path    =   require('path');
+    var util    =   require('util');
     var _       =   require('lodash');
     var Chance  =   require('chance').Chance(Math.random);
 
     var pkg     =   require('./package.json');
-    var Game    =   require('./modules/game');
+    var Emcee   =   require('./modules/emcee');
 
-    var dline   =   _(50).times(function () { return '='; }).join('');
+    /**
+     * VARIABLES
+     * @var
+     */
+
+    var dline       =   _(50).times(function () { return '='; }).join('');
+
+    var dataSRC     =   undefined;
+    var dataInput   =   undefined;
+    var dataDefault =   path.resolve(pkg.options.dataSource.defaults.path, pkg.options.dataSource.defaults.file);
+    var Args        =   process.argv.slice(2);
+
+    //  If name of the source file passed as command-line parameter - use it
+    //  otherwise - get file name from package.json
+    dataSRC =   (Args.length && Args[0])
+                    ?   path.resolve(Args[0])
+                    :   dataDefault;
+    console.info('[Input]:\tDataSource used: [' + dataSRC + ']');
+
+    //  Read the contents of the input file into memory
+    if (fs.existsSync(dataSRC)) {
+        dataInput   =   (fs.readFileSync(dataSRC, pkg.options.dataSource.params)).trim().split('\n');
+    }
+
+    if ('undefined' == typeof dataInput) {
+        console.warn('[Input]:\tDataSource NOT EXIST. Stop execution.');
+        return false;
+    }
 
     /**
      * METHODS
      * @public
      */
 
-    /*  Gameplay  */
-    function start () {
-        console.log('\n' + dline + '\t' + 'START' + '\t' + dline + '\n');
-
-        //  Read the contents of the input file into memory
-        console.info('[Input]:\tSource file used: [' + pkg.options.files.input + ']');
-        var dataInput   =   (fs.readFileSync(pkg.options.files.input, pkg.options.files.params)).trim().split('\n');
-
-        //  Create an instance of the game controller
-        var oGame   =   new Game({id: Chance.guid(), verbose: true});
-
-        //  Setup game options and create Tower and enemies objects
-        oGame.setup(dataInput);
-        oGame.Tower.notify();
-
-        //  Main game loop
-        while (oGame.Result.state === null) {
-            oGame.nextTurn()
-                .playRound()
-                .checkState();
-        }
-        oGame.logResult();
-
-        //  Insights
-        if (oGame.Result.state === 1) {
-            getHint(dataInput);
-        }
-
-        console.log('\n' + dline + '\t' + 'END' + '\t' + dline);
-        _(2).times(function () { console.log('\n'); });
-    }
-
-    /*  Calculate firing range to win the game  */
-    function getHint (loData) {
-        var found       =   false;
-        var testRange   =   0;
-        var testData    =   _.concat([], loData);
-
-        //  Limit iterations count to avoid infinite loop
-        var iLimit      =   1000;
-
-        while (!found && testRange < iLimit) {
-            testData[0] =   ++testRange;
-
-            var testGame    =   new Game({id: Chance.guid(), verbose: false});
-            testGame.setup(testData);
-
-            //  Test game loop
-            while (testGame.Result.state === null) {
-                testGame.nextTurn()
-                    .playRound()
-                    .checkState();
-            }
-
-            if (testGame.Result.state === 0) {
-                found   =   true;
-                console.log('[HINT]:\t\tMinimal firing range to win this game is: [' + testGame.Tower.fireRange + 'm]');
-            }
-        }
-
-        if (testRange == iLimit) {
-            console.log('[WARNING]:\tIterations limit reached!');
-        }
-    }
-
-    /**
-     * EXPORTS
-     * @public
-     */
-
-    module.exports  =   start();
+    console.info('[Input]:\tType of data extracted from source:', typeof dataInput);
+    var gameEmcee   =   new Emcee ({id: Chance.guid(), verbose: true});
+    gameEmcee.runGame(dataInput);
 
 }).call(this);
 
