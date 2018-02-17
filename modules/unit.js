26ce016aff01062e6d926d7c9aca9f4e2654cf80
Baltrushaitis Tomas <tbaltrushaitis@gmail.com>
2018-02-17 07:21:13 +0200

    no message

--- modules/unit.js
+++ modules/unit.js
@@ -9,74 +9,75 @@
 
 (function () {
 
-  /**
-   * DEPENDENCIES
-   * @private
-   */
+    /**
+     * DEPENDENCIES
+     * @private
+     */
 
-  var _      = require('lodash');
-  var Chance = require('chance').Chance(Math.random);
+    var _       =   require('lodash');
+    var Chance  =   require('chance').Chance(Math.random);
 
-  /**
-   * CONSTRUCTOR
-   * @void
-   */
+    /**
+     * CONSTRUCTOR
+     * @void
+     */
 
-  var Unit = function Unit (opts) {
-    var self = this;
-    var defs = {
-          id:          Chance.hash()
-        , name:        'Bot-' + Chance.first()
-        , distCurrent: 0
-        , speed:       0
-        , killed:      false
-      };
+    var Unit    =   function Unit (opts) {
+        var self    =   this;
+        var defs    =   {
+                id:             Chance.hash()
+              , name:           'Bot-' + Chance.first()
+              , distCurrent:    0
+              , speed:          0
+              , killed:         false
+            };
 
-    _.extend(self, true, defs, opts || {});
-  };
+        _.extend(self, true, defs, opts || {});
+    };
 
-  /**
-   * PROTOTYPE
-   * @void
-   */
+    /**
+     * PROTOTYPE
+     * @void
+     */
 
-  Unit.prototype             = Object.create(Object.prototype);
-  Unit.prototype.constructor = Unit;
+    Unit.prototype              =   Object.create(Object.prototype);
+    Unit.prototype.constructor  =   Unit;
 
-  /**
-   * METHODS
-   * @public
-   */
+    /**
+     * METHODS
+     * @public
+     */
 
-  // Move
-  Unit.prototype.move = function () {
-    var self = this;
-    if (!self.killed) {
-      var currentDist = self.distCurrent;
-      var newDist = Math.max(0, currentDist - self.speed);
-      self.distCurrent = newDist;
-      if (currentDist !== newDist) {
-        var Msg =   'Move ' + self.speed + 'm ' + '[' + currentDist + 'm' + ' --> ' + newDist + 'm' + ']';
-        self.notify(Msg);
-      }
-    }
+    //  Move
+    Unit.prototype.move =   function () {
+        var self    =   this;
+        if (!self.killed) {
+            var currentDist =   self.distCurrent;
+            var newDist =   Math.max(0, currentDist - self.speed);
+            self.distCurrent    =   newDist;
+            if (currentDist !== newDist) {
+                var Msg =   'Move ' + self.speed + 'm ' + '[' + currentDist + 'm' + ' --> ' + newDist + 'm' + ']';
+                self.notify(Msg);
+            }
+        }
 
-    return self;
-  };
+        return self;
+    };
 
-  //  Notifier
-  Unit.prototype.notify = function (sText) {
-    var self = this;
-    return (self.verbose
-              ? console.log('\t[' + self.name + ']' + ':\t', (sText || 'Distance: ' + self.distCurrent + 'm; Speed: ' + self.speed + 'm; ' + (self.killed ? 'DEAD' : 'ALIVE')))
-              : true);
-  };
+    //  Notifier
+    Unit.prototype.notify   =   function (sText) {
+        var self    =   this;
+        return (self.verbose
+                    ?   console.log('\t[' + self.name + ']' + ':\t', (sText || 'Distance: ' + self.distCurrent + 'm; Speed: ' + self.speed + 'm; ' + (self.killed ? 'DEAD' : 'ALIVE')))
+                    :   true);
+    };
 
-  /**
-   * EXPORTS
-   * @public
-   */
+    /**
+     * EXPORTS
+     * @public
+     */
 
-  module.exports = Unit;
+    module.exports  =   Unit;
 
 }).call(this);
+
